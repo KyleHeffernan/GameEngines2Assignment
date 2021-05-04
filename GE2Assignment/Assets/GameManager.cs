087c4713114e335c8fe645a2ddd78d9710ffd07a
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-05-04 20:37:59 +0100

    Coroutine and ship spawning added

---
+++ GE2Assignment/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject mainCam;
+
+    public GameObject fleet1;
+
+    public GameObject fleet2;
+
+    public GameObject fleet3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFleet());
+
+    }
+
+    System.Collections.IEnumerator SpawnFleet()
+    {
+        yield return new WaitForSeconds(3.0f);
+        fleet1.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        fleet2.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        fleet3.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        mainCam.GetComponent<CameraFollow>().enabled = true;
+
+        
+
+    }
+
+
+}
