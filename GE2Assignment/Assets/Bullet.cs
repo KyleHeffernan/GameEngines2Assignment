ebb0a6f628aa5372a13905de4311c2625f34465b
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-05-05 23:31:35 +0100

    Added bullet variance

--- GE2Assignment/Assets/Bullet.cs
+++ GE2Assignment/Assets/Bullet.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 50;
+
+    public float bulletOffset;
     // Start is called before the first frame update
     void Start()
     {
+
+        //get current forward
+        Vector3 variedDirection = transform.forward.normalized;
+        //get variance
+        variedDirection.x += Random.Range(-bulletOffset, bulletOffset);
+        variedDirection.y += Random.Range(-bulletOffset, bulletOffset);
+        //apply back to original
+        transform.forward = variedDirection;
+
         Destroy(this.gameObject, 5);
     }
 
