2166ffdf463e5c3a1e74860e090bfaa128db9459
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2021-05-06 22:19:02 +0100

    Position changes

--- GE2Assignment/Assets/GameManager.cs
+++ GE2Assignment/Assets/GameManager.cs
@@ -43,7 +43,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
-        if(milano.transform.position.z < 330)
+        if(milano.transform.position.z < 400)
         {
             if(runOnce1 == true)
             {
